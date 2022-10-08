9cb63ca86037f19a647938faabe736d666173b88
Monu <monu270800@gmail.com>
2022-10-09 02:28:26 +0530

    commit weather forecast1

---
+++ components/TemperatureAndDetails.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+// import Image from 'next/image'
+import {
+  UilTemperature,
+  UilTear,
+  UilWind,
+  UilSun,
+  UilSunset,
+} from "@iconscout/react-unicons";
+import { formatToLocalTime, iconUrlFromCode } from "../services/weatherService";
+
+// function Inputs({setUnits,units }) {
+//   // const [city, setCity] = useState("");
+
+//   const handleUnitsChange = (e) => {
+//     const selectedUnit = e.currentTarget.name;
+//     if (units !== selectedUnit) setUnits(selectedUnit);
+//   };
+
+//   };
+
+
+function TemperatureAndDetails({
+   
+  weather: {
+    details,
+    icon,
+    temp,
+    temp_min,
+    temp_max,
+    sunrise,
+    sunset,
+    speed,
+    humidity,
+    feels_like,
+    timezone,
+  },
+  
+  
+}) {
+
+  // const handleUnitsChange = (e) => {
+  //   const selectedUnit = e.currentTarget.name;
+  //   if (units !== selectedUnit) setUnits(selectedUnit);
+  // }
+  
+  return (
+    <div>
+      <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+        <p>{details}</p>
+      </div>
+
+      <div className="flex flex-row items-center justify-between text-white py-3">
+        <img src={iconUrlFromCode(icon)} layout='fill' alt="" className=" w-20" />
+        <p className="text-5xl">{`${temp.toFixed()}°`}</p>
+
+        {/* <div className="flex flex-row w-1/4 items-center justify-center">
+        <button
+          name="metric"
+          className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleUnitsChange}
+        >
+          °C
+        </button>
+        <p className="text-xl text-white mx-1">|</p>
+        <button
+          name="imperial"
+          className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleUnitsChange}
+        >
+          °F
+        </button>
+      </div> */}
+        
+        <div className="flex flex-col space-y-2">
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTemperature size={18} className="mr-1" />
+            Real fell:
+            <span className="font-medium ml-1">{`${feels_like.toFixed()}°`}</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTear size={18} className="mr-1" />
+            Humidity:
+            <span className="font-medium ml-1">{`${humidity.toFixed()}%`}</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilWind size={18} className="mr-1" />
+            Wind:
+            <span className="font-medium ml-1">{`${speed.toFixed()} km/h`}</span>
+          </div>
+        </div>
+        
+      </div>
+
+      <div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+        <UilSun />
+        <p className="font-light">
+          Rise:{" "}
+          <span className="font-medium ml-1">
+            {formatToLocalTime(sunrise, timezone, "hh:mm a")}
+          </span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSunset />
+        <p className="font-light">
+          Set:{" "}
+          <span className="font-medium ml-1">
+            {formatToLocalTime(sunset, timezone, "hh:mm a")}
+          </span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSun />
+        <p className="font-light">
+          High:{" "}
+          <span className="font-medium ml-1">{`${temp_max.toFixed()}°`}</span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSun />
+        <p className="font-light">
+          Low:{" "}
+          <span className="font-medium ml-1">{`${temp_min.toFixed()}°`}</span>
+        </p>
+      </div>
+      
+    </div>
+  );
+}
+
+// export default {Inputs,TemperatureAndDetails};
+export default TemperatureAndDetails;
---
+++ components/TopButtons.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Image from 'next/image'
+import london from "/public/london.jpg"
+import mumbai from "/public/mumbai.jpg"
+import tokyo from "/public/tokyo.jpg"
+import sydney from "/public/sydney.jpg"
+import lucknow from "/public/lucknow.webp"
+
+function TopButtons({ setQuery }) {
+  const cities = [
+    {
+      id: 1,
+      title: "London",
+      src: london,
+    },
+    {
+      id: 2,
+      title: "Mumbai",
+      src: mumbai,
+    },
+    {
+      id: 3,
+      title: "Tokyo",
+      src: tokyo,
+    },
+    {
+      id: 4,
+      title: "Lucknow",
+      src: lucknow,
+    },
+    {
+      id: 5,
+      title: "Sydney",
+      src: sydney,
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <div className="h-60 w-32 rounded-md" key={city.id}>
+        <Image className="rounded-md" src={city.src}
+          alt=""
+          // layout="fill"
+          // objectFit="cover"
+          width={1200}
+          height={1400}
+           ></Image>
+        
+        <button
+          
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+          
+          
+        </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons;
